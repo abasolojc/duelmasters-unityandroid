44f5acbe4b608e9020f4cdbf6043e2c4a8a38f56
cesar_abasolo <abasolojc@gmail.com>
2014-03-02 17:48:53 -0500

    Added game details xml file Read sets to load from DMO.xml

--- DMO_Repository/DMO/Assets/Scripts/LoadingManager.cs
+++ DMO_Repository/DMO/Assets/Scripts/LoadingManager.cs
@@ -10,7 +10,7 @@ public class LoadingManager : MonoBehaviour
 	void Start ()
 	{
 		DMO.cardDictionary = new Dictionary<string, CardMetadata> ();
-		LoadSet ("DM-1 Base Set");
+		LoadAllSets ();
 		Debug.Log ("Card count: " + DMO.cardDictionary.Values.Count.ToString ());
 	}
 	
@@ -23,22 +23,35 @@ public class LoadingManager : MonoBehaviour
 	void LoadSet (string xmlFileName)
 	{
 		TextAsset xmlSource = (TextAsset)Resources.Load ("Sets/" + xmlFileName, typeof(TextAsset));
-		XmlNodeList xmlNodeList;
+		XmlNodeList cardNodeList;
 		XmlDocument xmlDoc = new XmlDocument ();
 		xmlDoc.LoadXml (xmlSource.text);
 		
 		string setName = xmlDoc.GetElementsByTagName ("cardset") [0].Attributes ["setname"].Value;
 		Set newSet = new Set (setName, xmlFileName);
 		
-		xmlNodeList = xmlDoc.GetElementsByTagName ("card");
-		for (int i = 0; i < xmlNodeList.Count; i++) {
-			string candidateName = xmlNodeList [i].Attributes ["name"].Value.ToUpper ();
+		cardNodeList = xmlDoc.GetElementsByTagName ("card");
+		for (int i = 0; i < cardNodeList.Count; i++) {
+			string candidateName = cardNodeList [i].Attributes ["name"].Value.ToUpper ();
 			if (DMO.cardDictionary.ContainsKey (candidateName)) {
 				DMO.cardDictionary [candidateName].AddSet (newSet);
 			} else {
 				DMO.cardDictionary.Add (candidateName,
-				new CardMetadata (xmlNodeList [i], newSet));
+				new CardMetadata (cardNodeList [i], newSet));
 			}
+		}
+	}
+	
+	void LoadAllSets ()
+	{
+		TextAsset xmlSource = (TextAsset)Resources.Load ("DMO", typeof(TextAsset));
+		XmlDocument xmlDoc = new XmlDocument ();
+		xmlDoc.LoadXml (xmlSource.text);
+		
+		XmlNodeList setNodeList = xmlDoc.GetElementsByTagName ("sets") [0].ChildNodes;
+		for (int i = 0; i < setNodeList.Count; i++) {
+			string xmlFileName = setNodeList [i].InnerText;
+			LoadSet (xmlFileName);
 		}
 	}
 }
